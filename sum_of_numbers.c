487925c8dec8fc9b25450d1fb468d82f084acd0a
nikitha1908 <76110504+nikitha1908@users.noreply.github.com>
2021-02-18 10:12:51 +0530

    Update sum_of_numbers.c

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -1,1 +1,16 @@
 //Write a program to find the sum of n different numbers using 4 functions
+#include <stdio.h>
+int main()
+ {
+    int n, i, sum = 0;
+    printf("Enter a value of n= ");
+    scanf("%d", &n);
+   for(i=0;i<=n;i++)
+ {
+    sum=sum+i;
+        }
+
+    printf("Sum = %d", sum);
+    return 0;
+}
+
