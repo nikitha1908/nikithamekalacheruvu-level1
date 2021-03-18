23dc35ef26613b8ff6cb32f8e8de399c4b90c60c
nikitha1908 <76110504+nikitha1908@users.noreply.github.com>
2021-03-18 13:52:13 +0530

    Update distance.c

--- distance.c
+++ distance.c
@@ -1,26 +1,29 @@
 //WAP to find the distance between two point using 4 functions.
 #include<stdio.h>
 #include<math.h>
-  float input()
+  float input(float *x1,float *y1,float *x2,float *y2)
  {
- float x1,x2,y1,y2;
-printf("enter the points/coordinates of (x1,y1)");
-scanf("%f%f",&x1,&y1);
-printf("enter the points /coordinates of,(x2,y2)");
-scanf("%f%f",&x2,&y2);
+printf("enter the coordinates of (x1,y1)");
+scanf("%f%f",x1,y1);
+printf("enter the coordinates of (x2,y2)");
+scanf("%f%f",x2,y2);
+
 }
- float distance(float x1,float y1,float x2,float y2)
+ float find_distance(float x1,float y1,float x2,float y2)
 {
-   distance=sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-  return 0;
+  int distance=sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+  return distance;
 }
 void output(float distance)
 {
- printf("distance between two pints %f="distance);
+ printf("distance between two points %f",distance);
+ 
 }
 int main()
 {
- intput();
+float x1,y1,x2,y2,distance;
+input(&x1,&y1,&x2,&y2);
+distance =find_distance(x1,y1,x2,y2);
 output(distance);
-distance(x1,y1,x2,y2);
 }
+
